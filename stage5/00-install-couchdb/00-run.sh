0f1ce2f9b9137efaa93d9a296c02b1e728a453d7
Mike Colbert <mike@mikecolbert.com>
2017-05-09 06:18:51 -0400

    lightly tested

--- stage5/00-install-couchdb/00-run.sh
+++ stage5/00-install-couchdb/00-run.sh
@@ -2,7 +2,6 @@
 
 install -m 644 files/couchdb.service ${ROOTFS_DIR}/etc/systemd/system/couchdb.service
 
-rm -rf ${ROOTFS_DIR}/tmp/couchdb
 install -d ${ROOTFS_DIR}/tmp/couchdb
 
 on_chroot <<EOF
@@ -15,25 +14,30 @@ on_chroot <<EOF
   apt-get install -y libmozjs185-1.0 libmozjs185-dev && \
   apt-get install -y libcurl4-openssl-dev libicu-dev && \
   \
-  useradd -d /home/couchdb couchdb; \
-  mkdir /home/couchdb;  \
-  chown couchdb:couchdb /home/couchdb &&\
-  \
   wget http://www.gtlib.gatech.edu/pub/apache/couchdb/source/2.0.0/apache-couchdb-2.0.0.tar.gz && \
   tar zxvf apache-couchdb-2.0.0.tar.gz && \
   cd apache-couchdb-2.0.0/ && \
   ./configure && \
   make release && \
   \
-  sed -i 's/;bind_address = 127.0.0.1/bind_address = 0.0.0.0/' /tmp/couchdb/apache-couchdb-2.0.0/rel/couchdb/etc/local.ini &&
+  sed -i 's/;bind_address = 127.0.0.1/bind_address = 0.0.0.0/' /tmp/couchdb/apache-couchdb-2.0.0/rel/couchdb/etc/local.ini && \
   \
   mkdir /opt/couchdb; \
-  cp -Rp /tmp/couchdb/apache-couchdb-2.0.0/rel/couchdb/* /opt/couchdb && \
+  cp -Rp /tmp/couchdb/apache-couchdb-2.0.0/rel/couchdb/* /opt/couchdb/ && \
+  \
+  useradd --system \
+          --no-create-home \
+          --shell /bin/bash \
+          --user-group \
+          --home-dir /opt/couchdb \
+          couchdb; \
+  \
   chown -R couchdb:couchdb /opt/couchdb && \
+  find /opt/couchdb -type d -exec chmod 0770 {} \; && \
+  chmod 0644 /opt/couchdb/etc/* && \
   \
-  rm -rf /tmp/couchdb && \
+  systemctl enable couchdb.service && \
   \
-  systemctl enable couchdb.service
+  rm -rf /tmp/couchdb
 EOF
 
-# TODO REVIEW -->> http://docs.couchdb.org/en/2.0.0/install/unix.html
